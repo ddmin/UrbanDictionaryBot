1b26e778faa59e0eb078abdf9dd33ab049c5a236
Daniel Min <32945331+ddmin@users.noreply.github.com>
2018-08-28 15:28:53 -0400

    Update UrbanDictBotReddit.py

--- UrbanDictBotReddit.py
+++ UrbanDictBotReddit.py
@@ -18,10 +18,20 @@ def authenticate():
 def run_bot(reddit, comment_id):
     print("Obtaining 25 comments...\n")
     for comment in reddit.subreddit('test').comments(limit=25):
+                
         if "!UrbanDictBot " in comment.body and comment.id not in comment_id and str(comment.body)[0:13] == '!UrbanDictBot':
             word = str(comment.body)[14:]
             print(f'Found word "{word}" in {comment.id}')
-            definition = word_lookup(word)
+
+            try:
+                definition = word_lookup(word)
+            except:
+                comment.reply('I can\'t find that word on Urban Dictionary.\n\n***\n\n^(Bleep-bloop, This action was performed by a bot.)')
+                print("Replied to comment " + comment.id)
+                with open("replied_to.txt", "a") as f:
+                    f.write(comment.id + "\n")
+                pass
+            
             source = 'https://www.urbandictionary.com/define.php?term='+word
             comment.reply(f"#**{word.capitalize()}**:\n\n**Definition**: *{definition}*\n\n[Source]({source})\n\n***\n\n^(Bleep-bloop, This action was performed by a bot.)")
             print("Replied to comment " + comment.id)
@@ -29,11 +39,7 @@ def run_bot(reddit, comment_id):
             with open("replied_to.txt", "a") as f:
                 f.write(comment.id + "\n")
                 
-        if '!UrbanDictBot' in comment.body and comment.id not in comment_id and str(comment.body)[0:13] != '!UrbanDictBot' and comment.author != reddit.user.me():
-            comment.reply('If you want to summon me make sure nothing comes before "!UrbanDictBot" in your comment. Blame my human for this inconvenience.\n\n***\n\n^(Bleep-bloop, This action was performed by a bot.)')
-            print("Replied to comment " + comment.id)
-            with open("replied_to.txt", "a") as f:
-                f.write(comment.id + "\n")
+        
                 
 def get_ids():
     with open("replied_to.txt", "r") as f:

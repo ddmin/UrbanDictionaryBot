e770cf7b10a89461d6f827616042c85f27c492a9
Daniel Min <32945331+ddmin@users.noreply.github.com>
2018-08-29 14:46:35 -0400

    Update UrbanDictBotReddit.py

--- UrbanDictBotReddit.py
+++ UrbanDictBotReddit.py
@@ -11,7 +11,7 @@ def authenticate():
                          password = config.password,
                          client_id = config.client_id,
                          client_secret = config.client_secret,
-                         user_agent = "ddmin's UrbanDicBot v1.4")
+                         user_agent = "ddmin's UrbanDicBot v1.7")
     print("Authenticated!\n")
     return reddit
 
@@ -19,21 +19,30 @@ def run_bot(reddit, comment_id):
     print("Obtaining 25 comments...\n")
     for comment in reddit.subreddit('test').comments(limit=25):
                 
-        if "!UrbanDictBot " in comment.body and comment.id not in comment_id and str(comment.body)[0:13] == '!UrbanDictBot':
-            word = str(comment.body)[14:]
+        if "!UrbanDictBot " in comment.body and comment.id not in comment_id:
+            word_list = str(comment.body).split()
+            for n in range(len(word_list)):
+                if word_list[n] == '!UrbanDictBot':
+                    break
+                
+            word = ''
+            for item in word_list[n+1:]:
+                word += item
+                if item != word_list[-1]:
+                    word += ' '
             print(f'Found word "{word}" in {comment.id}')
 
             try:
                 definition = word_lookup(word)
             except:
-                comment.reply('I can\'t find that word on Urban Dictionary.\n\n***\n\n^(Bleep-bloop, This action was performed by a bot.)')
+                comment.reply('I can\'t find that word on Urban Dictionary.\n\n***\n\n^(Bleep-bloop. I am a bot. | [Github](https://github.com/ddmin/UrbanDictionaryBot))')
                 print("Replied to comment " + comment.id)
                 with open("replied_to.txt", "a") as f:
                     f.write(comment.id + "\n")
                 pass
             
             source = 'https://www.urbandictionary.com/define.php?term='+word
-            comment.reply(f"#**{word.capitalize()}**:\n\n**Definition**: *{definition}*\n\n[Source]({source})\n\n***\n\n^(Bleep-bloop, This action was performed by a bot.)")
+            comment.reply(f"#**{word.capitalize()}**:\n\n**Definition**: *{definition}*\n\n[Source]({source})\n\n***\n\n^(Bleep-bloop. I am a bot. | [Github](https://github.com/ddmin/UrbanDictionaryBot))")
             print("Replied to comment " + comment.id)
 
             with open("replied_to.txt", "a") as f:
@@ -54,5 +63,5 @@ while True:
         comment_id = get_ids()
         run_bot(reddit, comment_id)
     except:
-        print('Error. Sleeping for 30 seconds\n')
-        time.sleep(30)
+        print('Error. Sleeping for 10 seconds\n')
+        time.sleep(10)

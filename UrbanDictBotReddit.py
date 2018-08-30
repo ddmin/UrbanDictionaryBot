1640395fe74c50824f7c11b55322256e7de9f015
Daniel Min <32945331+ddmin@users.noreply.github.com>
2018-08-30 15:25:56 -0400

    Update UrbanDictBotReddit.py

--- UrbanDictBotReddit.py
+++ UrbanDictBotReddit.py
@@ -11,13 +11,13 @@ def authenticate():
                          password = config.password,
                          client_id = config.client_id,
                          client_secret = config.client_secret,
-                         user_agent = "ddmin's UrbanDicBot v1.7")
+                         user_agent = "ddmin's UrbanDicBot v1.9")
     print("Authenticated!\n")
     return reddit
 
 def run_bot(reddit, comment_id):
     print("Obtaining 25 comments...\n")
-    for comment in reddit.subreddit('test').comments(limit=25):
+    for comment in reddit.subreddit('herricks').comments(limit=25):
                 
         if "!UrbanDictBot " in comment.body and comment.id not in comment_id:
             word_list = str(comment.body).split()
@@ -26,6 +26,8 @@ def run_bot(reddit, comment_id):
                     break
                 
             word = ''
+
+            
             for item in word_list[n+1:]:
                 word += item
                 if item != word_list[-1]:
@@ -33,20 +35,27 @@ def run_bot(reddit, comment_id):
             print(f'Found word "{word}" in {comment.id}')
 
             try:
-                definition = word_lookup(word)
+                if word != '':
+                    definition = word_lookup(word)
+                    source = 'https://www.urbandictionary.com/define.php?term='+word
+                    comment.reply(f"#**{word.capitalize()}**:\n\n**Definition**: *{definition}*\n\n[Source]({source})\n\n***\n\n^(Bleep-bloop. I am a bot. | [Github](https://github.com/ddmin/UrbanDictionaryBot))")
+                    print("Replied to comment " + comment.id)
+
+                    with open("replied_to.txt", "a") as f:
+                        f.write(comment.id + "\n")
+                else:
+                    comment.reply('I can\'t find that word on Urban Dictionary.\n\n***\n\n^(Bleep-bloop. I am a bot. | [Github](https://github.com/ddmin/UrbanDictionaryBot))')
+                    print("Replied to comment " + comment.id)
+                    with open("replied_to.txt", "a") as f:
+                        f.write(comment.id + "\n")
+                
             except:
                 comment.reply('I can\'t find that word on Urban Dictionary.\n\n***\n\n^(Bleep-bloop. I am a bot. | [Github](https://github.com/ddmin/UrbanDictionaryBot))')
                 print("Replied to comment " + comment.id)
                 with open("replied_to.txt", "a") as f:
                     f.write(comment.id + "\n")
-                pass
             
-            source = 'https://www.urbandictionary.com/define.php?term='+word
-            comment.reply(f"#**{word.capitalize()}**:\n\n**Definition**: *{definition}*\n\n[Source]({source})\n\n***\n\n^(Bleep-bloop. I am a bot. | [Github](https://github.com/ddmin/UrbanDictionaryBot))")
-            print("Replied to comment " + comment.id)
-
-            with open("replied_to.txt", "a") as f:
-                f.write(comment.id + "\n")
+            
                 
         
                 
